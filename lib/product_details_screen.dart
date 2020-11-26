7eb74a61a1e44ef10e555b96ff2c54b8eeb50f22
Captain Web <contact@captain-web.fr>
2020-11-26 23:56:35 +0100

    product details screen bottom

--- lib/product_details_screen.dart
+++ lib/product_details_screen.dart
@@ -22,6 +22,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
           )
         ],
       ),
+      backgroundColor: Colors.grey,
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           return SizedBox(
@@ -85,8 +86,95 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                   ),
                 ),
                 Container(
-                  color: Colors.red,
                   height: constraints.maxHeight * 0.65,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(22),
+                      topRight: Radius.circular(22),
+                    )
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 85.0, left: 25, right: 25),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                                child: Text("en stock")
+                            ),
+                            Expanded(
+                                child: DropdownButton()
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 22,),
+                        Text("Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair"),
+                        SizedBox(height: 22,),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.grey
+                                  ),
+                                  shape: BoxShape.circle
+                                ),
+                                child: IconButton(
+                                  icon: Icon(
+                                    Icons.remove,
+                                    size: 15,
+                                  )
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                                child: Text(
+                                  "1",
+                                  textAlign: TextAlign.center,)
+                            ),
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.grey
+                                    ),
+                                    shape: BoxShape.circle
+                                ),
+                                child: IconButton(
+                                    icon: Icon(
+                                      Icons.add,
+                                      size: 15,
+                                    )
+                                ),
+                              ),
+                            ),
+                            Spacer(),
+                            Expanded(
+                              flex: 8,
+                              child: RaisedButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18)
+                                ),
+                                child: Text(
+                                    "bouton",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15
+                                  ),
+                                ),
+                                color: Colors.grey,
+                                onPressed: () => print("rien"),
+                              ),
+                            )
+                          ],
+                        )
+
+                      ],
+                    ),
+                  ),
                 ),
                 Positioned(
                   top: (constraints.maxHeight * 0.6)*0.35,
